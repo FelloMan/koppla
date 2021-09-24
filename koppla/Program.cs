64d453e51965c4345d711ecfea09bb8798458e1e
Filip Oldin <filip.oldin@edu.varberg.se>
2021-09-24 10:49:14 +0200

    Add project files.

---
+++ koppla/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace koppla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
